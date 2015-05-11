a098a6cfb029ad6c657e509f922ef717082ec8b4
AMEJIA4384 <richard.mejia.valencia@gmail.com>
2015-05-11 12:23:14 -0500

    1

---
+++ prjCajePremios/src/modelo/Estudiante.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Estudiante {
+ private String codigo,password,nombre,apellido="";
+ private int puntos,grupo=0;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String password, String nombre, int puntos) {
+        this.codigo = codigo;
+        this.password = password;
+        this.nombre = nombre;
+        this.puntos = puntos;
+    }
+
+ 
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the puntos
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+    /**
+     * @param puntos the puntos to set
+     */
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    /**
+     * @return the grupo
+     */
+    public int getGrupo() {
+        return grupo;
+    }
+
+    /**
+     * @param grupo the grupo to set
+     */
+    public void setGrupo(int grupo) {
+        this.grupo = grupo;
+    }
+ 
+
+}
